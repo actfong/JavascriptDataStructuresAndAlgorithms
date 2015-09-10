6e5e7ce81ec0ddc0e247b70080905bd257ac9c2f
Alex Fong <actfong@gmail.com>
2015-09-10 08:39:08 +0200

    added second variation, looping from right to left

--- javascript/array/shifting/leftToRight.js
+++ javascript/array/shifting/leftToRight.js
@@ -1,15 +1,18 @@
 // The idea of this exercise to to shift all elements within an array
 // from left to right by one position, where the value of the last index
 // will be moved to the first index.
-// The constraint here is to achieve this without initializing 
-// another array where we could paste values from the original array.
+// The constraint here is to achieve this 
+// 1. Without initializing another array where we could paste values from the original array.
+// 2. Without resizing the given array
 
 // Example => shiftLeftToRight([1,2,3,4]) #=> [2,3,4,1]
+
+var numbers = [1,2,3,4,5,6,7,8,9]
 
 // Variation 1, iterating from left to right
 function shiftLeftToRight_1(arr){
   memo = null;
-  for (i = 0; i < arr.length; i++) {
+  for (i = 0; i < arr.length; i++){
     if(arr[i-1] == undefined){
       memo = arr[i];
       arr[i] = arr.slice(i - 1)[0];      
@@ -21,6 +24,24 @@ function shiftLeftToRight_1(arr){
   }
   return arr;
 }
-
-var numbers = [1,2,3,4,5,6,7,8,9]
 console.log(shiftLeftToRight_1(numbers));
+
+
+// Variation 2, iterating from right to left
+function shiftLeftToRight_2(arr){
+  lastValue = null;
+
+  for(i = (arr.length - 1); i >= 0; i--){
+    if(arr[i+1] == undefined){
+      lastValue = arr[i];
+      arr[i] = arr[i-1];
+    }else if(arr[i-1] == undefined){
+      arr[i] = lastValue;
+    }else{
+      arr[i] = arr[i-1];
+    }
+  }
+  return arr;
+}
+
+console.log(shiftLeftToRight_2(numbers));
--- javascript/array/shifting/rightToLeft.js
+++ javascript/array/shifting/rightToLeft.js
@@ -1,10 +1,13 @@
 // The idea of this exercise to to shift all elements within an array
 // from RIGHT to LEFT by one position, where the value of the first index
 // will be moved to the last index.
-// The constraint here is to achieve this without initializing 
-// another array where we could paste values from the original array.
+// The constraint here is to achieve this 
+// 1. Without initializing another array where we could paste values from the original array.
+// 2. Without resizing the given array
 
 // Example => shiftLeftToRight([1,2,3,4]) #=> [4,1,2,3]
+
+var numbers = [1,2,3,4,5,6,7,8,9]
 
 // Variation 1, iterating from left to right
 function shiftRightToLeft_1(arr){
@@ -24,5 +27,23 @@ function shiftRightToLeft_1(arr){
   return arr;
 }
 
-var numbers = [1,2,3,4,5,6,7,8,9]
 console.log(shiftRightToLeft_1(numbers));
+
+// Variation 1, iterating from right to left
+function shiftRightToLeft_2(arr){
+  memo = null;
+
+  for(i=(arr.length-1); i >=0; i--){
+    if(arr[i+1] == undefined){
+      memo = arr[i] 
+      arr[i] = arr[0]
+    }else{
+      current = arr[i]
+      arr[i] = memo
+      memo = current;
+    }
+  }
+  return arr;
+}
+
+console.log(shiftRightToLeft_2(numbers));
